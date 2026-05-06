bed5196fa8d2b7705e365cb2b44c8be39c23d59b
karol <kdenys141@gmail.com>
2026-05-06 02:17:30 +0200

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,25 +1,13 @@
-// W React 18+ ten import nie jest wymagany do JSX (nowy transform automatycznie go dodaje)
-// Zostawiamy go dla czytelności – wiadomo że to plik React
-import React from 'react'; // eslint-disable-line @typescript-eslint/no-unused-vars
-
-// ReactDOM obsługuje renderowanie Reacta w przeglądarce (wstrzykiwanie do DOM)
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-// BrowserRouter – umożliwia routing (React Router)
-// Dzięki niemu /projekty, /realizacja/5, /admin to osobne "strony" bez przeładowania
 import { BrowserRouter } from 'react-router-dom';
-
-// Główny komponent aplikacji – zawiera definicję wszystkich tras
 import App from './App';
-
-// Globalny plik CSS z Tailwind – musi być zaimportowany żeby style działały
 import './index.css';
 
-// Tworzymy "korzeń" Reacta – szukamy elementu <div id="root"> w index.html
-// i mówimy Reactowi żeby tam renderował całą aplikację
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // BrowserRouter opakowuje całą aplikację – dzięki temu routing działa wszędzie
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
+
+if (window.__hideSplash) window.__hideSplash();
